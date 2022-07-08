6371c641406725733efb82f32caaf789a8d1e757
Ishiguro Mizuki <s1290024@u-aizu.ac.jp>
2022-07-08 11:58:03 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+class HeadsOrTails{
+    HeadsOrTails(){
+	System.out.println("Tossing a coin...");
+    }
+    
+    boolean toss(){
+	Random r = new Random();
+	
+	return r.nextBoolean();
+    }
+    
+    public static void main(String[] args){
+	HeadsOrTails obj = new HeadsOrTails();
+	int heads = 0;
+	int tails = 0;
+
+	for(int i = 1; i <= 3; i++){
+	    System.out.print("Round " + i + ": ");
+	    
+	    if(obj.toss()){
+		System.out.println("Heads");
+		heads++;
+	    }
+	    
+	    else{
+		System.out.println("Tails");
+		tails++;
+	    }
+	}
+
+	System.out.println("Heads: " + heads + ", Tails: " + tails);
+    }
+}
